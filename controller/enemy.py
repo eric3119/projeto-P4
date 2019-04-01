865d23c55c4dd0833e161cf8cfb1b3a24073a125
Eric Coelho <eric.s.coelho@gmail.com>
2019-04-01 19:52:28 -0300

    comment enemy, main

--- controller/enemy.py
+++ controller/enemy.py
@@ -4,35 +4,52 @@ import random
 class Enemy:
 
     def __init__(self, sprites):
-        # self.enemies = []
-        self.rects = []
-        self.dead_num = 0
         
         self.sprites = sprites
+        
+        #############
+        ## controle do loop da animação
+        ######
         self.sprite_index = 0
-
         self.anim_time = 200
         self.anim_control = 0
 
+        #############
+        ## controle do delay de geração de inimigos
+        ######
         self.delta = 1000
         self.start_time = 0
-        self.max_speed = 5
-        self.max_qtd = 5
-        self.step = 3
+        
+        #self.max_speed = 5
 
+        self.max_qtd = 5 # quantidade de inimigos na tela
+        self.step = 3 # quantidade de avanço de pixels
+
+        self.dead_num = 0 # quantidade de inimigos destruidos
+
+        ##############
+        ## retangulos para detecção de colisões
+        #######
+        self.rects = []
 
     def destroy(self, rect):
+        ''' marca um inimigo eliminado para ser destruido '''
         i = self.rects.index(rect)
         self.rects[i]['dead'] = True
         self.dead_num += 1
     
-    def alive(self, rect):        
+    def alive(self, rect):
         return not rect['dead']
 
     def trigger(self, clock_ticks):
-        if self.max_qtd <= len(self.rects):
+        ''' gera novos inimigos '''
+        
+        ## verifica o limite de inimigos na tela
+        if self.max_qtd <= len(self.rects): 
             return
-        # speed=(random.random()+1)*max_speed
+        
+        ## delay para controlar a velocidade com que
+        # os inimigos aparecem
         if (clock_ticks - self.start_time) >= self.delta:
             self.start_time = clock_ticks
             randNum = random.random()*(SCREEN_SIZE[1]-self.sprites[0].get_width())            
@@ -43,10 +60,11 @@ class Enemy:
                 'dead':False,
             })            
     
-    def isOnScreen(self, position):
-        return True if position[1] < SCREEN_SIZE[1] else False
+    # def isOnScreen(self, position):
+    #     return True if position[1] < SCREEN_SIZE[1] else False
     
-    def isOnScreen2(self, item):
+    def isOnScreen(self, item):
+        ''' verifica se não foi destruido ou se saiu da tela '''
         return True if item['rect'].y < SCREEN_SIZE[1] and not item['dead'] else False
     
     def get_sprite(self):
@@ -57,16 +75,22 @@ class Enemy:
 
     def update(self, clock_ticks):
         
-        self.trigger(clock_ticks)
+        self.trigger(clock_ticks) # tenta gerar um novo inimigo
 
+        ##############
+        ## atualização da animação
+        #######
+        # delay de atualização da animação
         if (clock_ticks - self.anim_control) >= self.anim_time:
             self.anim_control = clock_ticks
             self.sprite_index += 1
             self.sprite_index %= len(self.sprites)
 
+        ##############
+        ## atualização da posição atual
+        #######
         for i in range(len(self.rects)):
-            # self.enemies[i] = (self.enemies[i][0], self.enemies[i][1]+self.step)
             self.rects[i]['rect'].x, self.rects[i]['rect'].y = (self.rects[i]['rect'].x, self.rects[i]['rect'].y+self.step)
         
-        # self.enemies = list(filter(lambda x: self.isOnScreen(x) and self.alive(x), self.enemies))
-        self.rects = list(filter(lambda x: self.isOnScreen2(x), self.rects))
+        # remove inimigos marcados ou que sairam da tela
+        self.rects = list(filter(lambda x: self.isOnScreen(x), self.rects))
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     game = Game()    
     continue_game = game.main_menu()
 
-    while continue_game:
+    while continue_game:## reinicia o jogo
         del game
         game = Game()
         continue_game = game.main_menu()
