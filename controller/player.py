abf052213e15acb4294cb2b69a74f8794656b6b1
Eric Coelho <eric.s.coelho@gmail.com>
2019-04-01 19:36:43 -0300

    comment player

--- controller/player.py
+++ controller/player.py
@@ -5,21 +5,36 @@ from .game import SCREEN_SIZE
 class Player():
     def __init__(self, sprites):
         self.sprites = sprites        
-        self.sprite_index = 0 # indice do loop da animação
+        
+        #############
+        ## controle do loop da animação
+        ######
+        self.sprite_index = 0
         self.anim_control = 0
         self.anim_delta = 10
+        ############
 
+        #############
+        ## vidas e escudo anti colisão
+        #######
         self.health = 5
-        self.shield_time = 0
-        self.delta = 2000
-        
-        self.step = 10
+        self.shield_time = 0 # tempo da ultima colisão
+        self.delta = 2000 # tempo de duração do escudo
+        #############
 
+        self.step = 10 # quantidade de avanço de pixels
+
+        ##############
+        ## posição inicial
+        #######
         self.xpos, self.ypos = (
             (SCREEN_SIZE[0]-sprites[0].get_width())//2,
             SCREEN_SIZE[1]-sprites[0].get_height()
             )
         
+        ##############
+        ## retangulo para detecção de colisões
+        #######
         self.rect = self.sprites[0].get_rect()
     
     def get_sprite_size(self):
@@ -39,13 +54,22 @@ class Player():
             self.health-=1
             self.shield_time = pygame.time.get_ticks()            
     
+    def is_alive(self):
+        return True if self.health > 0 else False
+    
     def update(self, direction):
-        
+        ##############
+        ## atualização da animação
+        #######
+        # delay de atualização da animação
         if (pygame.time.get_ticks() - self.anim_control) >= self.anim_delta:
-            self.anim_control = pygame.time.get_ticks()
+            self.anim_control = pygame.time.get_ticks() # 
             self.sprite_index += 1
             self.sprite_index %= len(self.sprites)
 
+        ##############
+        ## atualização da posição atual
+        #######
         if direction == pygame.K_UP and self.ypos > 0:
             self.ypos -= self.step
         elif direction == pygame.K_DOWN and self.ypos < SCREEN_SIZE[1]-self.sprites[0].get_height():
@@ -55,7 +79,7 @@ class Player():
         elif direction == pygame.K_RIGHT and self.xpos < SCREEN_SIZE[0]-self.sprites[0].get_width():
             self.xpos += self.step
         
+        ##############
+        ## atualização da deteçção de colisões
+        #######
         (self.rect.x, self.rect.y) = (self.xpos, self.ypos)
-
-    def is_alive(self):
-        return True if self.health > 0 else False
